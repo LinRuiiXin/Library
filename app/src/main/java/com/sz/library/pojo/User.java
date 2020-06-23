b2754db882930c5cb1c9ed68bf2a001d0d14ea3d
LinRuixin <1303058293@qq.com>
2020-06-23 09:20:09 +0800

    没什么

---
+++ app/src/main/java/com/sz/library/pojo/User.java
@@ -0,0 +1,52 @@
+package com.sz.library.pojo;
+
+import org.litepal.crud.DataSupport;
+
+public class User extends DataSupport {
+    private int id;
+    private String name;
+    private String password;
+    private String mail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
